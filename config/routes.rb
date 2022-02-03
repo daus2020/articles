0ad27ad4b6a54666a0268fed32fad272ffa90cfc
daniel <danielandresus@gmail.com>
2022-02-03 06:37:53 -0300

    comment controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
+
+  root "articles#index"
+  
   resources :articles do
     resources :comments
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "articles#index"
 end
