25df1ebee6f40b4c612b4034da49234792a88a02
daniel <danielandresus@gmail.com>
2022-02-09 10:53:51 -0300

    comments validations added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
 
+  validates :commenter, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { in: 6..120 }
 end
